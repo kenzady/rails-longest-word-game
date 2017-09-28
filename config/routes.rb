cbb9041ca93e0b122bffb3d60e3bce479c02f364
Kenza <kenza.dieye@gmail.com>
2017-09-28 18:11:59 +0200

    exo completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'game', to: "pages#game"
+  get 'score/:grid', to: "pages#score"
+  get 'score/:start_time', to: "pages#score"
+  get 'score/:guess', to: "pages#score"
+  get 'score', to: "pages#score"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
